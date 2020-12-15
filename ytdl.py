b5602b9605933a74326b9a45dd5c0e5f40a7292c
redjordan1202 <75704668+redjordan1202@users.noreply.github.com>
2020-12-14 20:10:12 -0800

    Update ytdl.py

--- ytdl.py
+++ ytdl.py
@@ -6,12 +6,39 @@ yt = None
 res = None
 stream = None
 file_path = None
-#see if the user wants to download audio or video. This way they can get both
+av_swtich = None
 
 
+#see if the user wants to download audio or video
 
+def av_select():
+    global av_switch
+    print('Would you like to download Video or Audio:')
+    while True:
+        av_switch = input('Please enter \'1\' for Video and \'2\' for Audio\n')
+        if av_switch not in ('1', '2'):
+            print('Pleae make a valid selection')
+            continue
+        else:
+            break
 
+#Download Audio Stream
 
+def audio_download():
+    global yt
+    global file_path
+    print('The Video has the following audio streams')
+    print(*yt.streams.filter(only_audio=True), sep='\n')
+    while True:
+        itag = input('Please Enter the Itag of the stream you want to download:\n')
+        try:
+            yt.streams.get_by_itag(str(itag)).download(output_path=str(file_path), filename_prefix='Audio-')
+            print('The audio stream has been downloaded')
+            return True
+        except:
+            print('You have entered an invalid Itag')
+            continue
+        
 #Define Download location 
 def download_location():
     global file_path
@@ -44,8 +71,7 @@ def resoultion_select():
     while True:
         res = input('Please enter a Resoultion:\n')
         try:
-            res_list = yt.streams.filter(resolution= str(res)).all()
-            print(*res_list, sep='\n')
+            res_list = yt.streams.filter(resolution= str(res))
             return False
         except:
             print('You have entered an invalid resoultion')
@@ -55,6 +81,9 @@ def resoultion_select():
 def download_stream():
     global file_path
     global yt
+    global res_list
+    print('The Video has the following streams')
+    print(*res_list, sep='\n')
     while True:
         itag = input('Please Enter the Itag of the stream you want to download:\n')
         try:
@@ -64,25 +93,18 @@ def download_stream():
         except:
             print('You have entered an invalid Itag')
             continue
-            
+
+
 def Main():
+    global av_switch
+    print('Welcome to the Youtube Video Downloader\n')
     download_location()
     video_select()
-    resoultion_select()
-    download_stream()
-    exit()
-
+    av_select()
+    if av_switch == '1':
+        resoultion_select()
+        download_stream()
+    else:
+        audio_download()
+    
 Main()
-
-
-
-
-""" To do list
-Add a function to change between audio and video downloading
-Possibly do batch downloading/playlist downloading audio and video
-clean up the code and make a proper Main function that does more then just call all the other functions in order.
-
-
-"""
-
-#test video https://www.youtube.com/watch?v=mODI4-cRhPE
